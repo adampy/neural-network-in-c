a332c1009ffbb9bcf1ec9ed0033a3f6f6a59d245
adampy <adamflint8@hotmail.co.uk>
2022-06-04 17:58:20 +0100

    Image memory allocation and freeing functions

---
+++ image.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "image.h"
+#include "err.h"
+
+int makeImage(image* img) {
+    img = malloc(sizeof(image));
+    if (img == NULL) {
+        return reportError(IMAGE_MALLOC_FAILED, "");
+    }
+    // Set attributes to 0
+    img->columns = 0;
+    img->rows = 0;
+    img->hasImageDataAllocated = 0;
+    img->numberOfRowsAllocated = 0;
+    return SUCCESS;
+}
+
+int allocateImageData(image* img) {
+    // Check rows and columns
+    if (img->rows == 0 || img->columns == 0) {
+        return reportError(MISC, "Rows and columns must be set before malloc");
+    }
+
+    // Allocate image data 2D array
+    img->imageData = calloc(img->rows, sizeof(unsigned char *));
+    if (img->imageData == NULL) {
+        return reportError(IMAGE_MALLOC_FAILED, "");
+    }
+    img->hasImageDataAllocated = 1;
+
+    // Allocate all image data rows
+    for (int i = 0; i < img->rows; i++) {
+        img->imageData[i] = calloc(img->columns, sizeof(unsigned char));
+        if (img->imageData[i] == NULL) {
+            return reportError(IMAGE_MALLOC_FAILED, "");
+        }
+        img->numberOfRowsAllocated++;
+    }
+
+    return SUCCESS;
+}
+
+int freeImageData(image* img) {
+    if (img->hasImageDataAllocated) {
+        // Free all rows allocated
+        for (int i = img->numberOfRowsAllocated - 1; i >= 0; i--) {
+            free(img->imageData[i]);
+        }
+        free(img->imageData);
+    }
+    return SUCCESS;
+}
--- image.h
+++ image.h
@@ -5,6 +5,24 @@ typedef struct _image {
     unsigned int rows;
     unsigned int columns;
     unsigned char** imageData;
-} Image;
+    unsigned int hasImageDataAllocated;
+    unsigned int numberOfRowsAllocated;
+} image;
+
+/**
+ * Allocates memory to a null pointer for an image
+ */
+int makeImage(image* img);
+
+/**
+ * Allocates memory for `img->imageData`, and the rows inside it.
+ * It requires that columns and rows is set, and not 0.
+ */
+int allocateImageData(image* img);
+
+/**
+ * Free all memory used by this `img`.
+ */
+int freeImageData(image* img);
 
 #endif // IMAGE
