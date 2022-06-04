14c10c185e35cd887b4dc40978541d0e0e579d4f
adampy <adamflint8@hotmail.co.uk>
2022-06-04 17:57:55 +0100

    Error module

--- err.c
+++ err.c
@@ -1,5 +1,56 @@
 #include <stdio.h>
+#include "err.h"
 
-void function() {
-    printf("This is an error function");
+// --- Error strings ---
+#define BAD_ARGUMENT_COUNT_ERROR_STRING "ERROR: Bad Argument Count"
+#define BAD_FILE_NAME_ERROR_STRING "ERROR: Bad Filename (%s)"
+#define BAD_MAGIC_NUMBER_ERROR_STRING "ERROR: Bad Magic Number (%s)"
+#define IMAGE_MALLOC_FAILED_ERROR_STRING "ERROR: Image Malloc Failed"
+#define BAD_DATA_ERROR_STRING "ERROR: Bad Data (%s)"
+#define OUTPUT_FAILED_ERROR_STRING "ERROR: Output Failed (%s)"
+#define MISC_ERROR_STRING "ERROR: Miscellaneous (%s)"
+
+int reportError(int errorCode, char* string) {
+    // Delegate to appropriate function
+    switch (errorCode) {
+        case BAD_ARGUMENT_COUNT:
+            badArgumentCount();
+        case BAD_FILE_NAME:
+            badFileName(string);
+        case BAD_MAGIC_NUMBER:
+            badMagicNumber(string);
+        case IMAGE_MALLOC_FAILED:
+            imageMallocFailed();
+        case BAD_DATA:
+            badData(string);
+        case OUTPUT_FAILED:
+            outputFailed(string);
+        case MISC:
+            miscError(string);
+        default:
+            break;
+    }
+    return errorCode;
+}
+
+void badArgumentCount() {
+    printf(BAD_ARGUMENT_COUNT_ERROR_STRING);
+}
+void badFileName(char* string) {
+    printf(BAD_FILE_NAME_ERROR_STRING, string);
+}
+void badMagicNumber(char* string) {
+    printf(BAD_MAGIC_NUMBER_ERROR_STRING, string);
+}
+void imageMallocFailed() {
+    printf(IMAGE_MALLOC_FAILED_ERROR_STRING);
+}
+void badData(char* string) {
+    printf(BAD_DATA_ERROR_STRING, string);
+}
+void outputFailed(char* string) {
+    printf(OUTPUT_FAILED_ERROR_STRING, string);
+}
+void miscError(char* string) {
+    printf(MISC_ERROR_STRING, string);
 }
--- err.h
+++ err.h
@@ -1,6 +1,29 @@
-#ifndef ERROR
-#define ERROR
+#ifndef ERR
+#define ERR
 
-void function();
+typedef enum _error {
+    SUCCESS = 0,
+    BAD_ARGUMENT_COUNT = 1,
+    BAD_FILE_NAME = 2,
+    BAD_MAGIC_NUMBER = 3,
+    IMAGE_MALLOC_FAILED = 4,
+    BAD_DATA = 5,
+    OUTPUT_FAILED = 6,
+    MISC = 100
+} error;
 
-#endif // ERROR
+/**
+ * Delegates printing an error to the appropriate function. `string` is
+ * provided when necessary but is not guarantee. It returns `errorCode`.
+ */
+int reportError(int errorCode, char* string);
+
+void badArgumentCount();
+void badFileName();
+void badMagicNumber(char* string);
+void imageMallocFailed();
+void badData(char* string);
+void outputFailed(char* string);
+void miscError(char* string);
+
+#endif // ERR
