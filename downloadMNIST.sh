6e0c02780f8b2758b5f03646963368af94dfb9ab
adampy <adamflint8@hotmail.co.uk>
2022-06-15 19:26:22 +0100

    `run.sh` now downloads dataset if not present

---
+++ downloadMNIST.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+URL="http://yann.lecun.com/exdb/mnist/";
+trainingImages="train-images-idx3-ubyte";
+trainingLabels="train-labels-idx1-ubyte";
+testingImages="t10k-images-idx3-ubyte";
+testingLabels="t10k-labels-idx1-ubyte";
+datasets=("${trainingImages}" "${trainingLabels}" "${testingImages}" "${testingLabels}");
+
+echo "Downloading MNIST dataset...";
+
+# Make dataset directory and enter it
+if [ ! -d "dataset" ]; then
+    mkdir dataset;
+fi
+
+# Check every data piece is present, download it if not
+cd dataset;
+for i in {0..3}; do
+    dataPiece="${datasets[i]}";
+    if [ ! -f "${dataPiece}" ]; then
+        wget "${URL}${dataPiece}.gz";
+        gunzip "${dataPiece}.gz";
+    fi
+done
+cd ..;
+
+echo "MNIST dataset downloaded!";
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 make clean;
 make;
+./downloadMNIST.sh;
 ./main dataset/train-images-idx3-ubyte dataset/train-labels-idx1-ubyte dataset/t10k-images-idx3-ubyte dataset/t10k-labels-idx1-ubyte;
